d4b6da560edb39f79135c19727a374f364139d17
jinhanmei <jinhan.mei@mail.utronto.ca>
2020-03-30 15:24:48 +0800

    final draft of the assignment

--- hw2_code.py
+++ hw2_code.py
@@ -17,9 +17,7 @@ print("The correspoding eigenvoector is: ", np.around(pd_eigenvector[0], 2), ", 
       ", ", np.around(pd_eigenvector[2], 2), "\n")
 
 
-# Q1.3
-# Since we see that all the eigenvalue of the pd_Matrix is positive, (3.41, 2, 0.59 > 0), then the matrix is indeed
-# positive definite.
+
 
 
 # Problem 2
@@ -40,13 +38,19 @@ u = np.array([s1, s2, s3])
 
 cov_u = np.cov(u)
 
+print("con_u is :\n", cov_u, "\n")
+
 print("Covariance matrix of 100,000 sampled data from normal distribution is close to Identity matrix: ",
       np.allclose(np.around(cov_u, 2), np.identity(3), rtol=1e-2), "\n")
 # Since the diagonal of the covariance matrix is the variance of the sampled data, ie \sigma_{nn} = var(x_{n}), and all the data are sampled from standard normal distribution, that is, all the variance of x_{nn} = 1, and the diagonal of the covariance matrix is 1. Besides, since \sigma_{nm} = cov(x_{n},x_{m}), and all three X are sampled independently, so when m \neq n, cov(x_{n},x_{m}) \to 0, that is, all the matrix besides the diagonal approaches 0 when the sample size goes large enough. In conclusion, the covariance matrix is approximately the identity matrix.
 
 
 # 2.3
-# ?
+v = np.matmul(L, u)
+cov_v = np.cov(v)
+print("con_v is :\n", cov_v, "\n")
+
+print("Covariance matrix of Lu is close to A: ",
+      np.allclose(np.around(cov_v, 2), pd_Matrix, rtol=1e-2), "\n")
 
 # 3.1
-
